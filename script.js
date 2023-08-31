5f4ec465df776fa5d4eb4c0e49bf6b1fc29f64e5
Matthew Cham (s3822875) <s3822875@student.rmit.edu.au>
2023-08-31 17:37:42 +1000

    Created simple home, sign up and sign in pages

---
+++ script.js
@@ -0,0 +1,15 @@
+function signUp() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
+}
+
+function signIn() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    console.log(`Username: ${username}, Password: ${password}`);
+}
+  
