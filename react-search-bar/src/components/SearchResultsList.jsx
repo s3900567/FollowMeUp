cd30fccd742bd7c72b04fc65278f724825afad8c
YU JI <S3725496@student.rmit.edu.au>
2023-09-28 11:20:22 +1000

    search-bar

---
+++ react-search-bar/src/components/SearchResultsList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./SearchResultsList.css";
+import { SearchResult } from './SearchResult';
+
+export const SearchResultsList = ({results}) => {
+    return(
+        <div className="results-list">
+            {results.map((results,id)=>{
+                    return<SearchResult result={results} key={id}/>;
+                })}
+            
+
+        </div>
+    );
+    
+};
